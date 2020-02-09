f9e400c14c77844dba7f446491e849786dc820c9
powerkim <powerkim@etri.re.kr>
2020-02-09 12:10:55 +0900

    update

--- kubectl/shortening-kubectl.sh
+++ kubectl/shortening-kubectl.sh
@@ -1,10 +1,14 @@
 ## shortening of K8S CLI
-alias descnodes='kubectl describe nodes'
-alias descpods='kubectl describe pods'
+alias lsnode='kubectl get nodes -o wide'
+alias descnode='kubectl describe nodes'
+
+alias lspod='kubectl get pods -o wide'
+alias descpod='kubectl describe pods'
+
+alias lsdeploy='kubectl get deployments -o wide'
+alias descdeploy='kubectl describe deployments'
+
+alias lssvc='kubectl get svc -o wide'
 alias descsvc='kubectl describe svc'
 
-alias lsnodes='kubectl get nodes -o wide'
-alias lspods='kubectl get pods -o wide'
-alias lssvc='kubectl get svc -o wide'
-
-alias img='kubectl get -o custom-columns=CONTAINER:.spec.containers[0].name,IMAGE:.spec.containers[0].image pod'
+alias lsimg='kubectl -o custom-columns=CONTAINER:.spec.containers[*].name,IMAGE:.spec.containers[*].image get pods'
