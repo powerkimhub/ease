f3b5cb1ca0d88ed6539559ce5c079d0b0aa16c5a
ByoungSeob Kim <46367962+powerkimhub@users.noreply.github.com>
2020-03-20 22:04:02 +0900

    Update install-nfs-server.sh

--- easy-NFS/install-nfs-server.sh
+++ easy-NFS/install-nfs-server.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 ########
-# Install NFS Server on Ubuntu
+# Install & setup NFS Server on Ubuntu
 # 2020.03.20 by powerkim
 ########
 apt-get install -y nfs-common nfs-kernel-server portmap
-mkdir /nfs
-echo "/nfs  *(rw,sync,no_root_squash,no_all_squash,no_subtree_check)" >> /etc/exports
+mkdir /nfs/cloud-driver-libs
+echo "/nfs/cloud-driver-libs  *(rw,sync,no_root_squash,no_all_squash,no_subtree_check)" >> /etc/exports
 exportfs -r
 service nfs-kernel-server restart
 exportfs -v
