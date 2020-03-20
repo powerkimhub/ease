6cad0d1cd19434f6ac47f88f506783388ff0b226
ByoungSeob Kim <46367962+powerkimhub@users.noreply.github.com>
2020-03-20 21:57:32 +0900

    Create install-nfs-server.sh

---
+++ easy-NFS/install-nfs-server.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+########
+# Install NFS Server on Ubuntu
+# 2020.03.20 by powerkim
+########
+apt-get install nfs-common nfs-kernel-server portmap
+mkdir /nfs
+echo "/nfs  *(rw,sync,no_root_squash,no_all_squash,no_subtree_check)" >> /etc/exports
+exportfs -r
+service nfs-kernel-server restart
+exportfs -v
